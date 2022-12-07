961c556f80e0de345a54c1bde82a2bd9bc846a03
Vikas <vikaschoudharyfr@gmail.com>
2022-12-07 21:39:50 +0530

    Add constructor

--- script.js
+++ script.js
@@ -10,7 +10,20 @@ const Player = (name, sign) => {
 
 const gameBoard = (() => {
     const boardStatus = [];
-    return {boardStatus}
+    const askName = () => {
+        const name = prompt('what is your name');
+    }
+    const initiateGame = () => {
+        const name1 = askName();
+        const player1 = Player(name1, 'x');
+        const name2 = askName();
+        const player2 = Player(name2, '0');
+        return player1
+    }
+    const endGame = () => {
+
+    }
+    return {boardStatus, initiateGame, endGame, player1, player2}
 })();
 
  
